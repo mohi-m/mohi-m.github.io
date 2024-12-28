c6f260486a126b8529b0b6fb8d2359ef4bb3df13
mohi-m <mohi25081998@gmail.com>
2024-12-28 14:24:26 -0600

    bug fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mohi-m.github.io/',
   plugins: [react(), ghPages()],
   optimizeDeps: {
     exclude: ['lucide-react'],
